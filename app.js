f0eb034ac78202506136a23fbeeba476fef4c5cc
yarocruz <yarocruz@gmail.com>
2020-07-16 21:02:45 -0400

    beginning to understand

--- app.js
+++ app.js
@@ -1,13 +1,27 @@
-let websiteUrl = 'https://www.taniarascia.com/rss.xml';
+const RSS_URL = `https://codepen.io/picks/feed`;
 
-fetch(websiteUrl)
-    .then(res => {
-        res.text().then(htmlText => {
-            var domParser = new DOMParser();
-            let doc = domParser.parseFromString(htmlText, 'text/html');
-            console.log(doc);
-            var feedUrl = doc.querySelector('link').href;
-            console.log(feedUrl);
+fetch(RSS_URL)
+    .then(response => response.text())
+    .then(str => new window.DOMParser().parseFromString(str, 'text/xml'))
+    .then(data => {
+        console.log(data);
+        const items = data.querySelectorAll('item');
+        let html = ``;
+        items.forEach(el => {
+            html += `
+                <article>
+                    <img src="${el.querySelector("link").innerHTML}/image/large.png" alt="">
+                    <h2>
+                        <a href="${el.querySelector("link").innerHTML}" target="_blank" rel="noopener">
+                            ${el.querySelector('title').innerHTML}
+                        </a>
+                    </h2>
+                </article>        
+                      
+            
+            `;
         })
-    }).catch((err) => console.log(err));
+        console.log(html);
+        document.body.insertAdjacentHTML('beforeend', html);
+    });
 
